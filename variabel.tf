963b833e0d8b43dafd4a8e9865e73a4682efc6f3
nishita197 <nishitaardsn@gmail.com>
2022-11-08 22:11:28 +0530

    Changed

--- variabel.tf
+++ variabel.tf
@@ -1,9 +1,9 @@
-# variable "access_key" {
-#   type = string
-# }
-# variable "secret_key" {
-#   type = string
-# }
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
 
 variable "aws-region" {
   description = "The AWS region"
