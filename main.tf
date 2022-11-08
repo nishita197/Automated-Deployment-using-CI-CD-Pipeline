905f9089ad49769f2cb0cb0e5fa5f34761ebbf3f
nishita197 <nishitaardsn@gmail.com>
2022-11-09 00:09:32 +0530

    Changed

--- main.tf
+++ main.tf
@@ -95,14 +95,14 @@ resource "aws_instance" "instance" {
  
   key_name                    = "${var.instance-key-name != "" ? var.instance-key-name : ""}"
   associate_public_ip_address = "${var.instance-associate-public-ip}"
- user_data = << EOF
-#! /bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "The page was created by the user data" | sudo tee /var/www/html/index.html
-EOF
+  user_data = <<-EOF
+  #! /bin/bash
+  sudo apt-get update
+  sudo apt-get install -y apache2
+  sudo systemctl start apache2
+  sudo systemctl enable apache2
+  echo "The page was created by the user data" || sudo tee /var/www/html/index.html
+  EOF
 	# user_data = << EOF
 	# 	#! /bin/bash
   #   sudo apt-get update
