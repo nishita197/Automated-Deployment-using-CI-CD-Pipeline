cc68f4de8860a0d5a7478f5b544e5c2a9b434a72
nishita197 <nishitaardsn@gmail.com>
2022-11-08 23:43:34 +0530

    Changed

--- script.sh
+++ script.sh
@@ -1,20 +1,20 @@
-# #!/bin/bash
-# yum update -y
-# yum install -y httpd.x86_64
-# systemctl start httpd.service
-# systemctl enable httpd.service
-# sudo su
-# echo “Hello everyone” > /var/www/html/index.html
-
 #!/bin/bash
 yum update -y
-amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-yum install -y httpd mariadb-server
-systemctl start httpd
-systemctl enable httpd
-usermod -a -G apache ec2-user
-chown -R ec2-user:apache /var/www
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} \;
-find /var/www -type f -exec chmod 0664 {} \;
-echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+sudo su
+echo "Hello everyone" >> /var/www/html/index.html
+
+# #!/bin/bash
+# yum update -y
+# amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+# yum install -y httpd mariadb-server
+# systemctl start httpd
+# systemctl enable httpd
+# usermod -a -G apache ec2-user
+# chown -R ec2-user:apache /var/www
+# chmod 2775 /var/www
+# find /var/www -type d -exec chmod 2775 {} \;
+# find /var/www -type f -exec chmod 0664 {} \;
+# echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
