637aa97df216fa100c81812de111b8e089307a26
nishita197 <nishitaardsn@gmail.com>
2022-11-08 23:37:24 +0530

    Changed

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_instance" "instance" {
 		sudo apt-get install -y apache2
 		sudo systemctl start apache2
 		sudo systemctl enable apache2
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+		echo ?<h1>Deployed via Terraform</h1>? | sudo tee /var/www/html/index.html
 	EOF
 
   # user_data = "${file("script.sh")}"
